c05cddf55d71dede00e7ed3e514ca7b301416979
Muhammad Hasan <hasan@sirus.dev>
2019-10-14 11:11:21 +0700

    menambah unit test untuk fungsi tambah

---
+++ src/calculator.spec.ts
@@ -0,0 +1,10 @@
+import { expect } from 'chai';
+import { add } from './calculator';
+
+describe('calculator', function(){
+  describe('add', function(){
+    it('harusnya melakukan fungsi tambah', function(){
+      expect(add(1,2)).to.be.eq(3);
+    });
+  });
+})
